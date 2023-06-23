f680e7a348b6f589506cff8b4435ab11d383e75b
Aishat452 <aishadeperfectly@gmail.com>
2023-06-23 12:59:24 +0100

    connecting to d backend

--- bankingSystem/bankingSystem/urls.py
+++ bankingSystem/bankingSystem/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.index_view, name='index'),  # added to the root
     path('admin/', admin.site.urls),
 ]
