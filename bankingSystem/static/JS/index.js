540f6414bbf8d0466f30d0d329d578eeb67bd73a
Aishat452 <aishadeperfectly@gmail.com>
2023-07-01 13:02:54 +0100

    static files serving

---
+++ bankingSystem/static/JS/index.js
@@ -0,0 +1,29 @@
+// $(document).ready(function() {
+//     $(".hero").addClass("h1-animate h4-animate p-animate");
+//   });
+
+
+// document.getElementById('registration-form').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent form submission
+
+//     var form = event.target;
+//     var validationMessage = document.getElementById('validation-message');
+
+//     // Validate form fields
+//     if (!form.checkValidity()) {
+//         validationMessage.textContent = 'Please fill out all required fields.';
+//         validationMessage.className = 'alert error';
+//         return;
+//     }
+
+//     // Form submission is successful
+//     validationMessage.textContent = 'Form submitted successfully!';
+//     validationMessage.className = 'alert success';
+
+//     // Further actions (e.g., AJAX submission)
+//     // ...
+// });
+
+
+
+  
