04c545977e594673f65ac664046ec789818c95d4
Aishat452 <aishadeperfectly@gmail.com>
2023-07-04 02:54:24 +0100

    connected to postgres database for use

--- bankingSystem/bankingSystem/settings.py
+++ bankingSystem/bankingSystem/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'bankingSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'banking_system',
+        'USER':'postgres',
+        'PASSWORD':'Adeladun001%',
+        'HOST':'localhost',
+        'PORT':5000
     }
 }
 
