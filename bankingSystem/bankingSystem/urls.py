78ea2a72773328a55c404fcc2319ed2f350e2743
Aishat452 <aishadeperfectly@gmail.com>
2023-07-05 02:04:57 +0100

    dashboard setup

--- bankingSystem/bankingSystem/urls.py
+++ bankingSystem/bankingSystem/urls.py
@@ -35,23 +35,31 @@ urlpatterns = [
     path('customers-login/business-login/', views.business_login_view, name='business-login'),  # Business account login
 
     path('customers-login/investor-login/', views.investor_login_view, name='investor-login'),  # Investor account login
-    path('admin-login/', views.admin_login_view, name='admin-login'), # Admin account login
 
-    path('admin-home/', views.admin_mainpage_view, name='admin-homepage'), # Admin main page
+    path('admin-login/', views.admin_login_view,
+    name='admin-login'),  # Admin account login
 
-    path('admin-home/', views.admin_mainpage_view, name='admin-homepage'),  # Admin main page
+    path('admin-home/', views.admin_mainpage_view,
+    name='admin-homepage'),  # Admin main page
 
-    path('customers-list/', views.customers_list_view, name='customers-list'),  # Customers list
+    path('customers-list/', views.customers_list_view,
+    name='customers-list'),  # Customers list
 
     path('single-personal-page/', views.single_personal_page_view, name='single-personal-page'),  # Single personal customer page
 
     path('single-business-page/', views.single_business_page_view, name='single-business-page'),  # Single business customer page
 
     path('single-investor-page/', views.single_investor_page_view, name='single-investor-page'),  # Single investor customer page
+
+    path('personal-dashboard/', views.personal_dashboard_view,
+    name='personal-dashboard'),  # Personal dashboard page
     
-    path('transfer-page/', views.transfer_page_view, name='transfer-page'),  # Transfer page
+    path('business-dashboard/', views.business_dashboard_view,
+    name='business-dashboard'),  # Business dashboard page
+    
+    path('investor-dashboard/', views.investor_dashboard_view,
+    name='investor-dashboard'),  # Investor dashboard page
 
     # MAIN BACKEND DEV LOGIN
     path('admin/', admin.site.urls),
 ]
-
