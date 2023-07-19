cf27cd7723a23350633212018f499eae7783333e
Aishat452 <aishadeperfectly@gmail.com>
2023-07-19 13:42:29 +0100

    able to create and store database

--- bankingSystem/banking_System/models.py
+++ bankingSystem/banking_System/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# personal account reg
+# personal accounts registration
+
 class PersonalAccount(models.Model):
     fullname = models.CharField(max_length=255)
     email = models.EmailField()
@@ -14,15 +15,38 @@ class PersonalAccount(models.Model):
     class Meta:
         db_table = 'personal_account_reg'
 
-# business account reg 
+
+# business accounts registration
+
 class BusinessAccount(models.Model):
+    fullname = models.CharField(max_length=255)
+    email = models.EmailField()
+    user_password = models.CharField(max_length=255)
+    user_address = models.CharField(max_length=255)
+    user_address2 = models.CharField(max_length=255)
+    user_city = models.CharField(max_length=255)
+    user_state = models.CharField(max_length=255)
+    user_zipcode = models.CharField(max_length=10)
+
     class Meta:
         db_table = 'business_account_reg'
 
-# investor account reg
+
+# investors accounts registration
+
 class InvestorAccount(models.Model):
+    fullname = models.CharField(max_length=255)
+    email = models.EmailField()
+    user_password = models.CharField(max_length=255)
+    user_address = models.CharField(max_length=255)
+    user_address2 = models.CharField(max_length=255)
+    user_city = models.CharField(max_length=255)
+    user_state = models.CharField(max_length=255)
+    user_zipcode = models.CharField(max_length=10)
+
     class Meta:
         db_table = 'investors_account_reg'
+
 
 
 
