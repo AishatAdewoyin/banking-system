c16387a5d0a913899a482ad8b8defc0b1f7804fb
Aishat452 <aishadeperfectly@gmail.com>
2023-07-22 09:50:43 +0100

    creating custom user models

--- bankingSystem/banking_System/models.py
+++ bankingSystem/banking_System/models.py
@@ -21,56 +21,6 @@ class User(AbstractUser):
         return super().save(*args, **kwargs)
 
 
-
-
-
-
-
-# class CustomUserManager(BaseUserManager):
-#     def _create_user(self, email, password=None, **extra_fields):
-#         if not email:
-#             raise ValueError("Kindly provide an email address")
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-
-#     def create_user(self, email=None, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', False)
-#         extra_fields.setdefault('is_superuser', False)
-#         return self.create_user(email, password, **extra_fields)
-
-#     def create_superuser(self, email=None, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         return self.create_user(email, password, **extra_fields)
-
-# class User(AbstractBaseUser, PermissionsMixin):
-#     email = models.EmailField(blank=True, default='', unique=True)
-#     name = models.CharField(max_length=255, blank=True, default='')
-
-#     is_active = models.BooleanField(default=True)
-#     is_superuser = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-
-#     object = CustomUserManager()
-
-#     USERNAME_FIELD = 'email'
-#     EMAIL_FIELD = 'email'
-#     REQUIRED_FIELDS = []
-
-#     class Meta:
-#         verbose_name = 'User'
-#         verbose_name_plural = 'Users'
-
-#     def get_full_name(self):
-#         return self.name
-
-#     def get_shortform_name(self):
-#         return self.name or self.email.split('@')[0]
-
-
 # # personal accounts registration
 
 # class PersonalAccount(models.Model):
