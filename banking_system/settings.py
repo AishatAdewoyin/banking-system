d65ce983e91a5f8e77792346f5f8e7c027b81b31
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-08-12 16:27:24 +0100

    new branch

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -114,6 +114,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
